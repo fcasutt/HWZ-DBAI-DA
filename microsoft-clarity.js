8912dc9acc8bc29bb7eea67770dc2cc6d591b9c3
Fabio Casutt <50875486+fcasutt@users.noreply.github.com>
2024-02-24 16:14:18 +0100

    Update microsoft-clarity.js
    New Project ID for Tealium Ecommerce

--- microsoft-clarity.js
+++ microsoft-clarity.js
@@ -3,5 +3,5 @@
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "l6r6xady4l");
+    })(window, document, "clarity", "script", "l6u7hkilbs");
 </script>
