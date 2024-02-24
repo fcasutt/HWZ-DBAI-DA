f3182b66398814f6d6c332d5482fdc4e06d8217c
Fabio Casutt <50875486+fcasutt@users.noreply.github.com>
2024-02-24 20:36:30 +0100

    Create GTM-JS-ecommerce.js

---
+++ GTM-JS-ecommerce.js
@@ -0,0 +1,8 @@
+function(){
+  return { items: {{JS - items}},
+    currency: {{DL - order_currency}},
+    transaction_id: {{DL - order_id}},
+    value: {{DL - order_total}},
+    tax: {{DL - order_tax}}
+  }
+}
