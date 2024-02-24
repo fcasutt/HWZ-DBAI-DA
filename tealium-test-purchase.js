49375939f88ffccd48e44f8a175f3a2af5326f16
Fabio Casutt <50875486+fcasutt@users.noreply.github.com>
2024-02-24 22:56:20 +0100

    Create tealium-test-purchase.js

---
+++ tealium-test-purchase.js
@@ -0,0 +1,340 @@
+{
+  event: "purchase",
+  gtm: {
+    uniqueEventId: 7,
+    start: 1708790909973,
+    priorityId: undefined,
+    allowlist: undefined,
+    blocklist: undefined,
+    whitelist: undefined,
+    blacklist: undefined
+  },
+  tagTypeBlacklist: undefined,
+  site_region: "en_US",
+  site_currency: "USD",
+  page_name: "cart success",
+  page_type: "cart",
+  order_id: "145032915",
+  order_discount: "0.00",
+  order_subtotal: "750.00",
+  order_shipping: "0.00",
+  order_tax: "61.88",
+  order_payment_type: "Cash On Delivery",
+  order_total: "811.88",
+  order_currency: "USD",
+  customer_id: "",
+  product_id: ["406", "337"],
+  product_sku: ["msj013", "ace000"],
+  product_name: ["Linen Blazer", "Aviator Sunglasses"],
+  product_brand: [null, null],
+  product_category: [],
+  product_unit_price: ["455.00", "295.00"],
+  product_list_price: ["455.00", "295.00"],
+  product_price: ["455.00", "295.00"],
+  product_original_price: ["455.00", "295.00"],
+  product_quantity: ["1", "1"],
+  product_discount: ["0.00", "0.00"],
+  product_discounts: [
+    {
+      product_id: "406",
+      total_discount: "0.00",
+      discounts: [
+        {rule: "29", quantity: "0", amount: "0.00", type: "by_percent"}
+      ]
+    },
+    {
+      product_id: "337",
+      total_discount: "0.00",
+      discounts: [
+        {rule: "29", quantity: "0", amount: "0.00", type: "by_percent"}
+      ]
+    }
+  ],
+  customer_first_name: "asdf",
+  customer_last_name: "asdf",
+  customer_phone_number: "",
+  tealium_event: "purchase",
+  bdid: "MUEybpsHOrA2J4zK",
+  meta: {
+    description: "Default Description",
+    keywords: "Magento, Varien, E-commerce",
+    robots: "INDEX,FOLLOW",
+    viewport: "initial-scale=1.0, width=device-width"
+  },
+  dom: {
+    referrer: "https://ecommerce.tealiumdemo.com/checkout/onepage/",
+    title: "TealiumEcommerce Demo",
+    domain: "ecommerce.tealiumdemo.com",
+    query_string: "",
+    hash: "",
+    url: "https://ecommerce.tealiumdemo.com/checkout/onepage/success/",
+    pathname: "/checkout/onepage/success/",
+    viewport_height: 906,
+    viewport_width: 1569
+  },
+  ut: {
+    domain: "tealiumdemo.com",
+    version: "ut4.46.202402220913",
+    event: "view",
+    visitor_id: "018d886eddf7000fd22a75befd1d05075002206d00ac2",
+    session_id: "1708789284523",
+    account: "tealiumlabs",
+    profile: "commerce",
+    env: "prod"
+  },
+  tealium_visitor_id: "018d886eddf7000fd22a75befd1d05075002406d00ac2",
+  tealium_session_id: "1708789284523",
+  tealium_session_number: "4",
+  tealium_session_event_number: "14",
+  tealium_datasource: "",
+  tealium_account: "tealiumlabs",
+  tealium_profile: "commerce",
+  tealium_environment: "prod",
+  tealium_random: "5386395460484261",
+  tealium_library_name: "utag.js",
+  tealium_library_version: "4.46.0",
+  tealium_timestamp_epoch: 1708790909,
+  tealium_timestamp_utc: "2024-02-24T16:08:29.789Z",
+  tealium_timestamp_local: "2024-02-24T17:08:29.789",
+  js_page: {PERSISTENT: 1},
+  va: {
+    metrics: {
+      15: 2,
+      16: 2,
+      21: 4,
+      22: 44,
+      25: 52.66755,
+      26: 17.55585,
+      28: 2.2857142857142856,
+      29: 1.75,
+      5090: 985.08,
+      5133: 1,
+      5135: 985.08,
+      5181: 23.042053125,
+      5308: 985.08,
+      5341: 985.08,
+      5354: 145032914,
+      6397: 0,
+      6399: 0.05756204861111111,
+      6403: 0,
+      6436: 1,
+      6442: 0.09536234953703704,
+      6446: 910,
+      6460: 910,
+      6464: 985.08,
+      6468: 910,
+      6472: 985.08,
+      6488: 985.08,
+      6500: 1,
+      6502: 910,
+      6512: 910,
+      6522: 985.08,
+      6530: 1,
+      6540: 985.08,
+      6552: 4,
+      6556: 4,
+      6558: 910,
+      6560: 985.08,
+      7753: 9,
+      13431: 88,
+      18485: 0.02
+    },
+    dates: {
+      23: 1707390984521,
+      24: 1708782511664,
+      6401: 1708784311664,
+      6542: 1708781045718,
+      audience_tealium-education_data-access-public_106_count_ts: 170878
+                                                                  928517
+                                                                  0,
+      audience_tealium-education_data-access-public_121_count_ts: 170878
+                                                                  928517
+                                                                  0,
+      audience_tealium-education_data-access-public_129_count_ts: 170879
+                                                                  075566
+                                                                  2,
+      audience_tealium-education_data-access-public_113_count_ts: 170878
+                                                                  928517
+                                                                  0,
+      audience_tealium-education_data-access-public_112_count_ts: 170878
+                                                                  928517
+                                                                  0,
+      audience_tealium-education_data-access-public_140_count_ts: 170878
+                                                                  928517
+                                                                  0,
+      audience_tealium-education_data-access-public_139_count_ts: 170878
+                                                                  928517
+                                                                  0,
+      audience_tealium-education_data-access-public_120_count_ts: 170878
+                                                                  928517
+                                                                  0,
+      audience_tealiumlabs_commerce_129_count_ts: 1708790826479,
+      audience_tealiumlabs_commerce_121_count_ts: 1708789285025,
+      audience_tealiumlabs_commerce_106_count_ts: 1708789285025,
+      audience_tealiumlabs_commerce_148_count_ts: 1708789285025,
+      audience_tealiumlabs_commerce_113_count_ts: 1708789285025,
+      audience_tealiumlabs_commerce_120_count_ts: 1708789285025,
+      audience_tealiumlabs_commerce_146_count_ts: 1708790701440,
+      audience_tealiumlabs_commerce_149_count_ts: 1708790701440
+    },
+    properties: {
+      17: "https://ecommerce.tealiumdemo.com/checkout/onepage/",
+      54: "Mac desktop",
+      56: "Chrome",
+      58: "Mac OS X",
+      60: "browser",
+      62: "Chrome",
+      5095: "W98k0xgrW2Ld4ZuO5KSbGRyqsauLr8Rj",
+      5128: "[Linen Blazer]",
+      5166: "985.08",
+      5192: "[Linen Blazer]",
+      5194: "Mac desktop",
+      5224: "https://ecommerce.tealiumdemo.com/men/linen-blazer.html",
+      5227: "https://ecommerce.tealiumdemo.com/checkout/onepage/",
+      5234: "No Category",
+      5278: "0770A056734BB3B5-08537CCF0B083B62",
+      5427: "GA1.1.1535513416.1707390984",
+      6524: "Eyewear",
+      7552: "018d886eddf7000fd22a75befd1d05075002206d00ac2commerce",
+      profile: "commerce",
+      account: "tealiumlabs"
+    },
+    flags: {
+      27: true,
+      5097: true,
+      5106: true,
+      5108: false,
+      5168: true,
+      5252: false,
+      6534: true,
+      8472: true
+    },
+    property_sets: {
+      5280: ["Linen Blazer"],
+      5282: ["Chelsea Tee", "Linen Blazer"],
+      8006: ["No Category", "Men", "Eyewear"]
+    },
+    metric_sets: {
+      55: {Mac desktop: 3},
+      57: {Chrome: 3},
+      59: {Mac OS X: 3},
+      61: {browser: 3},
+      63: {Chrome: 3},
+      5129: {[Chelsea Tee]: 2, [Linen Blazer]: 6},
+      5193: {
+        [Chelsea Tee]: 2,
+        [Linen Blazer]: 13,
+        '[]': 2,
+        [Elizabeth Knit Top]: 1,
+        [Linen Blazer, Aviator Sunglasses]: 2
+      },
+      5195: {Mac desktop: 44},
+      5225: {
+        https://ecommerce．tealiumdemo．com/men/linen-blazer．html: 1
+      },
+      5235: {No Category: 38, Men: 4, Accessories: 2, Eyewear: 2}
+    },
+    current_visit: {
+      metrics: {
+        7: 13,
+        12: 25.690316666666668,
+        80: 2.2857142857142856,
+        5300: 12,
+        6458: 2,
+        6528: 2
+      },
+      dates: {10: 1708789285025, 11: 1708790826444},
+      properties: {
+        5: "https://ecommerce.tealiumdemo.com/checkout/cart/",
+        44: "Chrome",
+        45: "Mac OS X",
+        46: "Mac desktop",
+        47: "browser",
+        48: "Chrome",
+        5125: "Linen Blazer and Your Aviator Sunglasses"
+      },
+      flags: {13: true, 6476: false, 6480: false, 7842: true},
+      property_sets: {
+        49: ["Chrome"],
+        50: ["Mac OS X"],
+        51: ["Mac desktop"],
+        52: ["browser"],
+        53: ["Chrome"]
+      }
+    },
+    badges: {
+      30: true,
+      5127: true,
+      5140: true,
+      5145: true,
+      5221: true,
+      5328: true,
+      6470: true,
+      6550: true
+    },
+    audiences: {
+      tealium-education_data-access-public_140: "Product Fan",
+      tealium-education_data-access-public_120: "Purchasers",
+      tealium-education_data-access-public_121: "Visitors - Unknown",
+      tealium-education_data-access-public_106: "All Users - For Splunk",
+      tealium-education_data-access-public_139: "Online Purchaser",
+      tealium-education_data-access-public_112: "Mens Only Fan",
+      tealium-education_data-access-public_113: "Adobe Visitor Update",
+      tealiumlabs_commerce_121: "Visitors - Unknown",
+      tealiumlabs_commerce_113: "Adobe Visitor Update",
+      tealiumlabs_commerce_146: "Accessories Fan",
+      tealiumlabs_commerce_148: "Thomas Test Audience",
+      tealiumlabs_commerce_106: "All Users - For Splunk",
+      tealiumlabs_commerce_149: "Thomas Test Audience Two - Less than" +
+                                " 1 day",
+      tealiumlabs_commerce_129: "Every Event",
+      tealiumlabs_commerce_120: "Purchasers"
+    },
+    last_visit_id: "e0bf0fded89bd20a2a6edf95f62477cafe78260bfd2cfcbef" +
+                   "aa4487dc17e6593"
+  },
+  audiencestream: "true",
+  category_name: "No Category",
+  ga_event_name: "purchase",
+  customer_country: "",
+  customer_state: "",
+  order_coupon_code: "",
+  order_shipping_type: "",
+  product_on_page: "",
+  product_image_url: "",
+  product_promo_code: "",
+  customer_zip: "",
+  customer_city: "",
+  _ccity: "",
+  _ccountry: "",
+  _ccurrency: "USD",
+  _ccustid: "",
+  _corder: "145032915",
+  _cpromo: "",
+  _cship: "0.00",
+  _cstate: "",
+  _cstore: "",
+  _csubtotal: "750.00",
+  _ctax: "61.88",
+  _ctotal: "811.88",
+  _ctype: "",
+  _czip: "",
+  _cprod: ["406", "337"],
+  _cprodname: ["Linen Blazer", "Aviator Sunglasses"],
+  _cbrand: [null, null],
+  _ccat: [],
+  _ccat2: [],
+  _cquan: ["1", "1"],
+  _cprice: ["455.00", "295.00"],
+  _csku: ["msj013", "ace000"],
+  _cpdisc: [],
+  gua_cid: "GA1.1.1535513416.1707390984",
+  gua_cid_as: "GA1.1.1535513416.1707390984",
+  adwords_pagetype: "purchase",
+  customer_first_last_name: "asdf asdf",
+  customer_first_last_name_encrypted: "c4defcd096da16177ecf7b184e411f" +
+                                      "48d58e85cb0e6bf4b7d110446cdf34" +
+                                      "05a5c1a3e81be72a0a6a36b3ac55f9" +
+                                      "3fdeaba90bfb738f16cc78baa82947" +
+                                      "8fa5962b"
+}
